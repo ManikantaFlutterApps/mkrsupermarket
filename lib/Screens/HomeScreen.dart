70b73730c665e5ad3c79d9a1289761a3952e5b17
manikantareddy <vmkrmca@gmail.com>
2024-04-12 08:03:07 +0530

    DashBoardScreen.dart

--- lib/Screens/HomeScreen.dart
+++ lib/Screens/HomeScreen.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -9,10 +8,110 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+
+  TextEditingController searchController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text("HOME",style: TextStyle(color: Colors.orangeAccent,fontWeight: FontWeight.bold,fontSize: 25),),
+    return Container(
+      color: Colors.grey[200],
+      child: SafeArea(
+        child: Column(
+          children: [
+            Expanded(
+              flex: 1,
+              child: Container(
+                width: double.infinity,
+                color: Colors.white,
+                child: const Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text("Sai Balaji Residency ,Nagundahalli Main Road , Bangalore",style: TextStyle(fontSize: 10,color: Colors.black,fontWeight: FontWeight.normal),),
+
+                    SizedBox(height: 5,),
+
+                    Text("6:00 AM - 11:00 PM",style: TextStyle(fontSize: 8,color: Colors.black,fontWeight: FontWeight.bold),),
+
+                  ],
+                ),
+              ),
+            ),
+
+            Expanded(
+              flex: 1,
+              child: Container(
+                color: Colors.white,
+                width: double.infinity,
+                child:  Card(
+                  elevation: 0,
+                  margin: const EdgeInsets.only(left: 10,right: 10),
+                  shape: RoundedRectangleBorder(
+                    side: const BorderSide(color: Colors.grey, width: 1),
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  color: Colors.transparent,
+                  child: Padding(
+                    padding: const EdgeInsets.only(left: 15,right: 15),
+                    child: TextField(
+                      controller: searchController,
+                      decoration: const InputDecoration(
+                        border: InputBorder.none,
+                        hintText: "What are you Looking for (Mangos , Orange,etc)"
+                      ),
+
+                    ),
+                  ),
+                ),
+              ),
+            ),
+
+            Expanded(
+              flex: 2,
+              child: Container(
+                color: Colors.white,
+                child: productsCard(),
+              ),
+            ),
+
+            Expanded(
+              flex: 2,
+              child: Container(
+                color: Colors.blueAccent,
+              ),
+            ),
+
+            Expanded(
+              flex: 7,
+              child: Container(
+                color: Colors.pinkAccent,
+              ),
+            ),
+
+          ],
+        ),
+      ),
     );
   }
+}
+
+Widget productsCard() {
+  return SizedBox(
+      height: 200.0,
+      child: ListView.builder(
+        scrollDirection: Axis.horizontal,
+        itemCount: 10,
+        itemBuilder: (BuildContext context, int i) =>
+            Card(
+              child: SizedBox(
+                width: 160.0,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: <Widget>[
+                    Image.asset('lib/images/g_image_one.png'),
+                  ],
+                ),
+              ),
+            ),
+      )
+  );
 }
